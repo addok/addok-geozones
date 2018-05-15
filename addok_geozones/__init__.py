c32e6fd4904b6b51b04b871bc8c1f6da4ce4ef44
Yohan Boniface <yb@enix.org>
2018-05-15 22:48:01 +0200

    Adapt to geozone new level format

--- addok_geozones/__init__.py
+++ addok_geozones/__init__.py
@@ -1,8 +1,8 @@
 def preconfigure(config):
     # Default configuration values.
-    config.GEOZONES_LEVELS = ['fr/region', 'fr/departement', 'fr/commune']
+    config.GEOZONES_LEVELS = ['fr:region', 'fr:departement', 'fr:commune']
     config.GEOZONES_MAX_IMPORTANCE = {
-        'fr/region': 11898502,
-        'fr/departement': 2830000,
-        'fr/commune': 2249975
+        'fr:region': 11898502,
+        'fr:departement': 2830000,
+        'fr:commune': 2249975
     }
